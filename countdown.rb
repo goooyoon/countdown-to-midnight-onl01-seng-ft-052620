6153a1d0d1808bbd647b64284d0cee4b84bd35c1
Grace Kim <goooyoon@gmail.com>
2020-05-27 23:02:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+n = 0
+while n < 12
+  puts "#{n} SECOND(S)!"
+  n += 1
+when n == 12
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+end
+
+x = 0
+while x < 6
+  x += 1
 end
