29dba09afab5ca60e166340414095e044d60f9f9
Grace Kim <goooyoon@gmail.com>
2020-05-27 23:08:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,21 +1,16 @@
-def countdown(from)
-  count = from
-  while count > 0
-    puts "#{count} SECOND(S)!"
-    count -= 1
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
-end
 
-def countdown_with_sleep(from)
-  count = from
-  while count > 0
-    puts "#{count} SECOND(S)!"
-    count -= 1
+def countdown_with_sleep(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
     sleep 1
+  number -= 1
   end
   return "HAPPY NEW YEAR!"
-end
-
-countdown(5)
+end	end
